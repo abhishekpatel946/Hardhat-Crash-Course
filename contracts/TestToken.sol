3a0673f9806464dc1742ebd26f7e39f8f8a2a6ec
Abhishek Patel <46045014+abhishekpatel946@users.noreply.github.com>
2022-02-24 12:21:55 +0530

    Created ERC20 TestToken & Compile the contract

---
+++ contracts/TestToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract TestToken {
+    string public name = "TestToken";
+    string public symbol = "TST";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000000;
+
+    address public owner;
+
+    mapping(address => uint256) public balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
